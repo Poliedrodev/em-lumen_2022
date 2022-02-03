9a96fc9bb186d5f81277ee6e499a4e64e63b72f5
Juliano de Arruda Fernandes <juliano.fernandes@sistemapoliedro.com.br>
2022-02-03 10:29:46 -0300

    boxes tooltips e titulo cap

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -85,8 +85,50 @@ $(document).ready(function() {
         });
         //Fim do script para o componente
         //Inicio do script para o componente
+        var emArteCont = $('.css_06_container_boxe_em_arte');
+        emArteCont.on('click', function(){
+            console.log('02');
+            var randomId = Math.floor(Math.random() * 100000) + 1;
+            
+            if($(this).find('input').attr('id').length == "0") {
+                console.log('ID alterado');
+                $(this).find('input').attr('id', randomId);
+            } else {
+                console.log('Ja contem ID');
+            }
+            
+            if($(this).find('label').attr('for').length == "0") {
+                console.log('FOR alterado');
+                $(this).find('label').attr('for', randomId);
+            } else {
+                console.log('Ja contem FOR');
+            }
+        });
+        //Fim do script para o componente
+        //Inicio do script para o componente
         var emBiologia = $('.css_06_boxe_em_biologia');
         emBiologia.on('click', function(){
+            console.log('02');
+            var randomId = Math.floor(Math.random() * 100000) + 1;
+            
+            if($(this).find('input').attr('id').length == "0") {
+                console.log('ID alterado');
+                $(this).find('input').attr('id', randomId);
+            } else {
+                console.log('Ja contem ID');
+            }
+            
+            if($(this).find('label').attr('for').length == "0") {
+                console.log('FOR alterado');
+                $(this).find('label').attr('for', randomId);
+            } else {
+                console.log('Ja contem FOR');
+            }
+        });
+        //Fim do script para o componente
+        //Inicio do script para o componente
+        var emBiologiaCont = $('.css_06_container_boxe_em_biologia');
+        emBiologiaCont.on('click', function(){
             console.log('02');
             var randomId = Math.floor(Math.random() * 100000) + 1;
             
@@ -127,8 +169,50 @@ $(document).ready(function() {
         });
         //Fim do script para o componente
         //Inicio do script para o componente
+        var emFilosofiaCont = $('.css_06_container_boxe_em_filosofia');
+        emFilosofiaCont.on('click', function(){
+            console.log('02');
+            var randomId = Math.floor(Math.random() * 100000) + 1;
+            
+            if($(this).find('input').attr('id').length == "0") {
+                console.log('ID alterado');
+                $(this).find('input').attr('id', randomId);
+            } else {
+                console.log('Ja contem ID');
+            }
+            
+            if($(this).find('label').attr('for').length == "0") {
+                console.log('FOR alterado');
+                $(this).find('label').attr('for', randomId);
+            } else {
+                console.log('Ja contem FOR');
+            }
+        });
+        //Fim do script para o componente
+        //Inicio do script para o componente
         var emFisica = $('.css_06_boxe_em_fisica');
         emFisica.on('click', function(){
+            console.log('02');
+            var randomId = Math.floor(Math.random() * 100000) + 1;
+            
+            if($(this).find('input').attr('id').length == "0") {
+                console.log('ID alterado');
+                $(this).find('input').attr('id', randomId);
+            } else {
+                console.log('Ja contem ID');
+            }
+            
+            if($(this).find('label').attr('for').length == "0") {
+                console.log('FOR alterado');
+                $(this).find('label').attr('for', randomId);
+            } else {
+                console.log('Ja contem FOR');
+            }
+        });
+        //Fim do script para o componente
+        //Inicio do script para o componente
+        var emFisicaCont = $('.css_06_container_boxe_em_fisica');
+        emFisicaCont.on('click', function(){
             console.log('02');
             var randomId = Math.floor(Math.random() * 100000) + 1;
             
@@ -169,8 +253,50 @@ $(document).ready(function() {
         });
         //Fim do script para o componente
         //Inicio do script para o componente
+        var emGeografiaCont = $('.css_06_container_boxe_em_geografia');
+        emGeografiaCont.on('click', function(){
+            console.log('02');
+            var randomId = Math.floor(Math.random() * 100000) + 1;
+            
+            if($(this).find('input').attr('id').length == "0") {
+                console.log('ID alterado');
+                $(this).find('input').attr('id', randomId);
+            } else {
+                console.log('Ja contem ID');
+            }
+            
+            if($(this).find('label').attr('for').length == "0") {
+                console.log('FOR alterado');
+                $(this).find('label').attr('for', randomId);
+            } else {
+                console.log('Ja contem FOR');
+            }
+        });
+        //Fim do script para o componente
+        //Inicio do script para o componente
         var emHistoria = $('.css_06_boxe_em_historia');
         emHistoria.on('click', function(){
+            console.log('02');
+            var randomId = Math.floor(Math.random() * 100000) + 1;
+            
+            if($(this).find('input').attr('id').length == "0") {
+                console.log('ID alterado');
+                $(this).find('input').attr('id', randomId);
+            } else {
+                console.log('Ja contem ID');
+            }
+            
+            if($(this).find('label').attr('for').length == "0") {
+                console.log('FOR alterado');
+                $(this).find('label').attr('for', randomId);
+            } else {
+                console.log('Ja contem FOR');
+            }
+        });
+        //Fim do script para o componente
+        //Inicio do script para o componente
+        var emHistoriaCont = $('.css_06_container_boxe_em_historia');
+        emHistoriaCont.on('click', function(){
             console.log('02');
             var randomId = Math.floor(Math.random() * 100000) + 1;
             
@@ -211,8 +337,50 @@ $(document).ready(function() {
         });
         //Fim do script para o componente
         //Inicio do script para o componente
+        var emLiteraturaCont = $('.css_06_container_boxe_em_literatura');
+        emLiteraturaCont.on('click', function(){
+            console.log('02');
+            var randomId = Math.floor(Math.random() * 100000) + 1;
+            
+            if($(this).find('input').attr('id').length == "0") {
+                console.log('ID alterado');
+                $(this).find('input').attr('id', randomId);
+            } else {
+                console.log('Ja contem ID');
+            }
+            
+            if($(this).find('label').attr('for').length == "0") {
+                console.log('FOR alterado');
+                $(this).find('label').attr('for', randomId);
+            } else {
+                console.log('Ja contem FOR');
+            }
+        });
+        //Fim do script para o componente
+        //Inicio do script para o componente
         var emLinguaInglesa = $('.css_06_boxe_em_lingua_inglesa');
         emLinguaInglesa.on('click', function(){
+            console.log('02');
+            var randomId = Math.floor(Math.random() * 100000) + 1;
+            
+            if($(this).find('input').attr('id').length == "0") {
+                console.log('ID alterado');
+                $(this).find('input').attr('id', randomId);
+            } else {
+                console.log('Ja contem ID');
+            }
+            
+            if($(this).find('label').attr('for').length == "0") {
+                console.log('FOR alterado');
+                $(this).find('label').attr('for', randomId);
+            } else {
+                console.log('Ja contem FOR');
+            }
+        });
+        //Fim do script para o componente
+        //Inicio do script para o componente
+        var emLinguaInglesaCont = $('.css_06_container_boxe_em_lingua_inglesa');
+        emLinguaInglesaCont.on('click', function(){
             console.log('02');
             var randomId = Math.floor(Math.random() * 100000) + 1;
             
@@ -253,8 +421,50 @@ $(document).ready(function() {
         });
         //Fim do script para o componente
         //Inicio do script para o componente
+        var emLinguaPortuguesaCont = $('.css_06_container_boxe_em_lingua_portuguesa');
+        emLinguaPortuguesaCont.on('click', function(){
+            console.log('02');
+            var randomId = Math.floor(Math.random() * 100000) + 1;
+            
+            if($(this).find('input').attr('id').length == "0") {
+                console.log('ID alterado');
+                $(this).find('input').attr('id', randomId);
+            } else {
+                console.log('Ja contem ID');
+            }
+            
+            if($(this).find('label').attr('for').length == "0") {
+                console.log('FOR alterado');
+                $(this).find('label').attr('for', randomId);
+            } else {
+                console.log('Ja contem FOR');
+            }
+        });
+        //Fim do script para o componente
+        //Inicio do script para o componente
         var emMatematica = $('.css_06_boxe_em_matematica');
         emMatematica.on('click', function(){
+            console.log('02');
+            var randomId = Math.floor(Math.random() * 100000) + 1;
+            
+            if($(this).find('input').attr('id').length == "0") {
+                console.log('ID alterado');
+                $(this).find('input').attr('id', randomId);
+            } else {
+                console.log('Ja contem ID');
+            }
+            
+            if($(this).find('label').attr('for').length == "0") {
+                console.log('FOR alterado');
+                $(this).find('label').attr('for', randomId);
+            } else {
+                console.log('Ja contem FOR');
+            }
+        });
+        //Fim do script para o componente
+        //Inicio do script para o componente
+        var emMatematicaCont = $('.css_06_container_boxe_em_matematica');
+        emMatematicaCont.on('click', function(){
             console.log('02');
             var randomId = Math.floor(Math.random() * 100000) + 1;
             
@@ -295,6 +505,27 @@ $(document).ready(function() {
         });
         //Fim do script para o componente
         //Inicio do script para o componente
+        var emProducaoDeTextoCont = $('.css_06_container_boxe_em_producao_de_texto');
+        emProducaoDeTextoCont.on('click', function(){
+            console.log('02');
+            var randomId = Math.floor(Math.random() * 100000) + 1;
+            
+            if($(this).find('input').attr('id').length == "0") {
+                console.log('ID alterado');
+                $(this).find('input').attr('id', randomId);
+            } else {
+                console.log('Ja contem ID');
+            }
+            
+            if($(this).find('label').attr('for').length == "0") {
+                console.log('FOR alterado');
+                $(this).find('label').attr('for', randomId);
+            } else {
+                console.log('Ja contem FOR');
+            }
+        });
+        //Fim do script para o componente
+        //Inicio do script para o componente
         var emQuimica = $('.css_06_boxe_em_quimica');
         emQuimica.on('click', function(){
             console.log('02');
@@ -316,8 +547,50 @@ $(document).ready(function() {
         });
         //Fim do script para o componente
         //Inicio do script para o componente
+        var emQuimicaCont = $('.css_06_container_boxe_em_quimica');
+        emQuimicaCont.on('click', function(){
+            console.log('02');
+            var randomId = Math.floor(Math.random() * 100000) + 1;
+            
+            if($(this).find('input').attr('id').length == "0") {
+                console.log('ID alterado');
+                $(this).find('input').attr('id', randomId);
+            } else {
+                console.log('Ja contem ID');
+            }
+            
+            if($(this).find('label').attr('for').length == "0") {
+                console.log('FOR alterado');
+                $(this).find('label').attr('for', randomId);
+            } else {
+                console.log('Ja contem FOR');
+            }
+        });
+        //Fim do script para o componente
+        //Inicio do script para o componente
         var otherAreas = $('.css_06_in_other_area');
         otherAreas.on('click', function(){
+            console.log('02');
+            var randomId = Math.floor(Math.random() * 100000) + 1;
+            
+            if($(this).find('input').attr('id').length == "0") {
+                console.log('ID alterado');
+                $(this).find('input').attr('id', randomId);
+            } else {
+                console.log('Ja contem ID');
+            }
+            
+            if($(this).find('label').attr('for').length == "0") {
+                console.log('FOR alterado');
+                $(this).find('label').attr('for', randomId);
+            } else {
+                console.log('Ja contem FOR');
+            }
+        });
+        //Fim do script para o componente
+        //Inicio do script para o componente
+        var otherAreasCont = $('.css_06_container_in_other_area');
+        otherAreasCont.on('click', function(){
             console.log('02');
             var randomId = Math.floor(Math.random() * 100000) + 1;
             
@@ -381,6 +654,27 @@ $(document).ready(function() {
         //Inicio do script para o componente
         var emSociologia = $('.css_06_boxe_em_sociologia');
         emSociologia.on('click', function(){
+            console.log('02');
+            var randomId = Math.floor(Math.random() * 100000) + 1;
+            
+            if($(this).find('input').attr('id').length == "0") {
+                console.log('ID alterado');
+                $(this).find('input').attr('id', randomId);
+            } else {
+                console.log('Ja contem ID');
+            }
+            
+            if($(this).find('label').attr('for').length == "0") {
+                console.log('FOR alterado');
+                $(this).find('label').attr('for', randomId);
+            } else {
+                console.log('Ja contem FOR');
+            }
+        });
+        //Fim do script para o componente
+        //Inicio do script para o componente
+        var emSociologiaCont = $('.css_06_container_boxe_em_sociologia');
+        emSociologiaCont.on('click', function(){
             console.log('02');
             var randomId = Math.floor(Math.random() * 100000) + 1;
             
